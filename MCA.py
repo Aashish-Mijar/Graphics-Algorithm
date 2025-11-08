be8835774b34ac039aca843c7c36fadc6f5ba3c9
rep-demo <aashishmijar44@gmail.com>
2025-11-08 20:08:37 +0545

    Function to implement algorithm

--- MCA.py
+++ MCA.py
@@ -6,3 +6,21 @@ def plot_circle_points(xc, yc, x, y, color):
              [yc+y, yc+y, yc-y, yc-y, yc+x, yc+x, yc-x, yc-x],
              'o', color=color)
 
+def midpoint_circle(xc, yc, r, color='blue', label=None):
+    """Midpoint Circle Drawing Algorithm"""
+    x = 0
+    y = r
+    p = 1 - r
+    plt.plot([], [], color=color, label=label)  # for legend
+
+    plot_circle_points(xc, yc, x, y, color)
+    while x < y:
+        x += 1
+        if p < 0:
+            p += 2 * x + 1
+        else:
+            y -= 1
+            p += 2 * (x - y) + 1
+        plot_circle_points(xc, yc, x, y, color)
+
+
