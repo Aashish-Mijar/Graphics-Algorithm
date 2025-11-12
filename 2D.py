925af1432170ed720f88b13e85febcbca06ce046
rep-demo <aashishmijar44@gmail.com>
2025-11-12 18:43:49 +0545

    X-axis and Y-axis

---
+++ 2D.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+# --- 2D Illustration ---
+plt.figure(figsize=(7, 7))
+plt.title("2D Illustration Example")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.grid(True)
+
+
