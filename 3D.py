706a568108e3a3e2cb63ad0d5cc9412a64ffd1ae
rep-demo <aashishmijar44@gmail.com>
2025-11-12 18:48:11 +0545

    Setting GUI

---
+++ 3D.py
@@ -0,0 +1,10 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+# --- 3D Illustration ---
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(111, projection='3d')
+ax.set_title("3D Illustration Example")
+
+
