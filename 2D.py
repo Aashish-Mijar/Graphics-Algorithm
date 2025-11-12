3a905b68b4c82f5afec12e2d5e47c1867b1b985e
rep-demo <aashishmijar44@gmail.com>
2025-11-12 18:45:41 +0545

    Circle

--- 2D.py
+++ 2D.py
@@ -16,3 +16,10 @@ rect_x = [1, 4, 4, 1, 1]
 rect_y = [1, 1, 3, 3, 1]
 plt.plot(rect_x, rect_y, color='blue', label='Rectangle')
 
+# Circle (using parametric form)
+import numpy as np
+theta = np.linspace(0, 2*np.pi, 100)
+x_circle = 2 + np.cos(theta)
+y_circle = 5 + np.sin(theta)
+plt.plot(x_circle, y_circle, color='green', label='Circle')
+
