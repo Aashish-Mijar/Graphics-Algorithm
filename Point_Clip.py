1ed9ac160958233ead218e2e1b6166264c47738c
rep-demo <aashishmijar44@gmail.com>
2025-11-17 21:47:13 +0545

    Taking x and y coordinate input

--- Point_Clip.py
+++ Point_Clip.py
@@ -5,3 +5,9 @@ xmin = float(input("Enter xmin: "))
 ymin = float(input("Enter ymin: "))
 xmax = float(input("Enter xmax: "))
 ymax = float(input("Enter ymax: "))
+
+
+# Input the point to be clipped
+x = float(input("Enter x-coordinate of the point: "))
+y = float(input("Enter y-coordinate of the point: "))
+
