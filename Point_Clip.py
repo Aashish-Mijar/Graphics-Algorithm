ffd1ed0881f60cd01e7649197c6625515fc47806
rep-demo <aashishmijar44@gmail.com>
2025-11-17 21:46:32 +0545

    Taking input x min-max and y min-max

---
+++ Point_Clip.py
@@ -0,0 +1,7 @@
+# Point Clipping Program in Python
+
+# Define clipping window boundaries
+xmin = float(input("Enter xmin: "))
+ymin = float(input("Enter ymin: "))
+xmax = float(input("Enter xmax: "))
+ymax = float(input("Enter ymax: "))
