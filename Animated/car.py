de7829fe05c1e70ad9d81fecdf3b1000a44432e2
rep-demo <aashishmijar44@gmail.com>
2025-12-15 21:18:45 +0545

    Mid point circle algo

--- Animated/car.py
+++ Animated/car.py
@@ -18,4 +18,28 @@ def dda_line(x1, y1, x2, y2):
 
     return points
 
+# ================= MIDPOINT CIRCLE ALGORITHM ==================
+def midpoint_circle(xc, yc, r):
+    points = []
+    x = 0
+    y = r
+    p = 1 - r
+
+    while x <= y:
+        points.extend([
+            (xc+x, yc+y), (xc-x, yc+y),
+            (xc+x, yc-y), (xc-x, yc-y),
+            (xc+y, yc+x), (xc-y, yc+x),
+            (xc+y, yc-x), (xc-y, yc-x)
+        ])
+
+        x += 1
+        if p < 0:
+            p += 2*x + 1
+        else:
+            y -= 1
+            p += 2*(x - y) + 1
+
+    return points
+
 
